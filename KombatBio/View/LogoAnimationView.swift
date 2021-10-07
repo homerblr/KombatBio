bc23df091cccd9b0f9d13a2891404ba13299a159
Mikhail Kisly <mikhail.kisly@itrexgroup.com>
2021-10-07 20:55:30 +0300

    some commit

---
+++ KombatBio/View/LogoAnimationView.swift
@@ -0,0 +1,33 @@
+//
+//  LogoAnimationView.swift
+//  KombatBio
+//
+//  Created by Mikhail Kisly on 5.03.21.
+//
+
+import UIKit
+import SwiftyGif
+
+class LogoAnimationView: UIView {
+
+    let logoGifImageView = try! UIImageView(gifImage: UIImage(gifName: "raidenGif.gif"), loopCount: 3)
+
+       override init(frame: CGRect) {
+           super.init(frame: frame)
+           commonInit()
+       }
+
+       required init?(coder aDecoder: NSCoder) {
+           super.init(coder: aDecoder)
+           commonInit()
+       }
+    
+    private func commonInit() {
+        backgroundColor = UIColor.black
+        addSubview(logoGifImageView)
+        logoGifImageView.contentMode = .scaleAspectFill
+        logoGifImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoGifImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        logoGifImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+    }
+}
